7b19b3ec0ed435d31c175f3751b1e40af40f0180
JP <potti.jordan@gmail.com>
2018-01-03 08:30:38 -0600

    Update py-alert.py

--- py-alert.py
+++ py-alert.py
@@ -7,8 +7,7 @@ import requests
 import subprocess
 import os
 
-time = datetime.datetime.fromtimestamp(1514903262).isoformat()
-
+time = datetime.datetime.now().strftime("%H:%M-%y-%m-%d")
 
 def print_banner():
     print('''\npy-alert.py is a tool written to expand the functionality of ElastAlert
@@ -19,9 +18,8 @@ def print_banner():
 def main():
     global arguments
     parser = ArgumentParser()
-    parser.add_argument("-T", dest="action",required=False,help="Action Type: Send Alert (S) or Data Write (D)")
-    parser.add_argument("-a", dest="detection",required=False,help="Alert Name")
-    parser.add_argument("-o", dest="outfile",required=False,help="Output file name")
+    parser.add_argument("-T", dest="action",required=True,help="Action Type: Send Alert (S) or Data Write (D)")
+    parser.add_argument("-a", dest="detection",required=True,help="Alert Name")
     parser.add_argument("-c", dest="host",required=False,help="Host to record")
     parser.add_argument("-S", dest="slack",required=False,help="Slack Web Hook")
     parser.add_argument("-t", dest="tripped",required=False,help="Number or Hosts needed to alert")
@@ -33,40 +31,33 @@ def main():
         sys.exit()
 
     arguments = parser.parse_args()
-    
+    outfile = '/tmp/'+arguments.detection
     if arguments.action == 'D':
-        with open (arguments.outfile, "a+") as out_file:
+        with open (outfile, "a+") as out_file:
             out_file.write(arguments.host+"\n")
-            #out_file.write("Alert Type: " + arguments.detection+"\n")
-            #out_file.write("Time: " + time)
 
     if arguments.action == 'S':
-        command = "sort /tmp/mimikatz | uniq -c | gawk '$1>=%s{print $2}'" %arguments.tripped
-        #print(command)
-        #output = os.popen("sort /tmp/mimikatz | uniq -c | gawk '$1>=5{print $2}'").read()
+        command = "head -50 %s | sort | uniq -c | gawk '$1>=%s{print $2}'" %(outfile,arguments.tripped) 
+        print(command)
         output = os.popen(command).read()
         if output != '':
             output = str(output)
             output = output.replace('b\'','')
             output = output.replace('\\n','')
-            out_file = open(arguments.outfile, 'w')
+            out_file = open(outfile, 'w')
             out_file.write("Host: " + output)
             out_file.write("Alert Type: " + arguments.detection+"\n")
             out_file.write("Time: " + time)
-        #try: 
-        #out_file.close()
-        out_file = open(arguments.outfile, 'r')
-        webhook_url = arguments.slack
-        slack_data = {"text":out_file.read()}
-        slack_data = str(slack_data)
-        slack_data = "payload="+slack_data
-        response = requests.post(
-            webhook_url, data=slack_data,
-            headers={'Content-Type': 'application/x-www-form-urlencoded'})
-        if response.status_code != 200:
-            raise ValueError('Request to slack returned an error %s, the response is: %s' % (response.status_code, response.text))
-        #except:
-        #    print("Error")
-        os.remove(arguments.outfile)
+            out_file = open(outfile, 'r')
+            webhook_url = arguments.slack
+            slack_data = {"text":out_file.read()}
+            slack_data = str(slack_data)
+            slack_data = "payload="+slack_data
+            response = requests.post(
+                webhook_url, data=slack_data,
+                headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            if response.status_code != 200:
+                raise ValueError('Request to slack returned an error %s, the response is: %s' % (response.status_code, response.text))
+            os.remove(outfile)
 
 main()
